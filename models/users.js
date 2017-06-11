6e103a0b39c5cfcff11795d23e567e6390f7b19a
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-06-11 20:27:51 +0300

    i dont know, what was done

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 import db from '../db';
 
 exports.checkLogin = (account, cb) => {
-    db.get().collection('users').find({name: account.login, password: account.password}).toArray((err, docs) => {
+    db.get().collection('users').find({login: account.login, password: account.password}).toArray((err, docs) => {
         cb(err, docs);
     })  
 }
